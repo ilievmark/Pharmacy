e62e6c3251f0f15d39c631d68f41b59e61739183
Mark Iliev <ilievmarik@gmail.com>
2021-06-30 01:50:40 +0300

    Added add period api with bl

---
+++ PharmacyServer/BL/Pharmacy.BL.Services.Impl/PerionService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Pharmacy.BL.Contract;
+using Pharmacy.DAL;
+using Pharmacy.DAL.Contract;
+
+namespace Pharmacy.BL.Services
+{
+    public class PerionService : IPerionService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PerionService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PeriodEntity> OpenPeriodAsync(DateTime start)
+        {
+            var periodRepository = _unitOfWork.GetRepository<PeriodEntity>();
+            var period = new PeriodEntity();
+
+            period.Start = start;
+
+            await periodRepository.AddAsync(period);
+
+            return period;
+        }
+    }
+}
---
+++ PharmacyServer/BL/Pharmacy.BL.Services/IPerionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Pharmacy.DAL;
+
+namespace Pharmacy.BL.Contract
+{
+    public interface IPerionService
+    {
+        Task<PeriodEntity> OpenPeriodAsync(DateTime start);
+    }
+}
