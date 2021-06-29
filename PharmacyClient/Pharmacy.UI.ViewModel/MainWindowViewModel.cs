d932b17bbba0ee18cba4e159a455cde57ee1c7c7
Mark Iliiev <miliiev@playtika.com>
2021-06-29 10:49:10 +0300

    Setup client infra

---
+++ PharmacyClient/Pharmacy.UI.ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace Pharmacy.UI.ViewModel
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            ViewPharmacy = new DelegateCommand(OnViewPharmacy);
+        }
+
+        public ICommand ViewPharmacy { get; }
+
+        private void OnViewPharmacy()
+        {
+            
+        }
+    }
+}
---
+++ PharmacyClient/Pharmacy/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Pharmacy.UI;
+using Pharmacy.UI.ViewModel;
+using Prism.Ioc;
+
+namespace Pharmacy
+{
+    public partial class App
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainWindow, MainWindowViewModel>();
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+    }
+}
